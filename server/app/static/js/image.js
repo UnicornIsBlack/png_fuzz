7923e3034433b1bd9c81c1da64bee321a93d3141
chai <304334894@qq.com>
2017-12-11 18:04:37 +0800

    update 17/12/11 18:04

---
+++ server/app/static/js/image.js
@@ -0,0 +1,25 @@
+function addImg(imageFiles)
+{
+    var div = document.getElementsByClassName("imageDiv")[0];
+
+    while( !imageFiles.atEnd() )
+    {
+        var img = document.createElement("img");
+        img.setAttribute("class","image");
+        img.src = "../image/" + imageFiles.item();
+        div.appendChild(img);
+        imageFiles.moveNext();
+    }
+}
+
+function start()
+{
+    alert("1");
+}
+
+$(document).ready(function(){
+    var car = [];
+    car.push("1");
+    car.push("2");
+    alert(car[1]);
+})
